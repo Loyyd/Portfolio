58d156bac0562a969c9fe61c4fceb5522e22fa12
Loyyd <konradkunkel@gmx.de>
2025-05-28 18:54:59 +0200

    ="true"

--- src/App.jsx
+++ src/App.jsx
@@ -220,7 +220,7 @@ function App() {
     return (
         <>
             {/* Embedded CSS Styles */}
-            <style jsx global>{`
+            <style jsx="true" global>{`
                 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap');
                 
                 * {
